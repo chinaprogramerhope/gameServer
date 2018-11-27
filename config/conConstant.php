57584e35de571bd9c880136d1b951aa53d5dd969
chinaprogramerhope <31737416+chinaprogramerhope@users.noreply.github.com>
2018-11-27 14:29:09 +0800

    Update conConstant.php

--- config/conConstant.php
+++ config/conConstant.php
@@ -15,6 +15,7 @@ class conConstant {
     // redis连接配置
     const redis_ip = '127.0.0.1';
     const redis_port = 6379;
+    const redis_pass = '';
 
     const default_redis_expire_time = 604800; // redis键默认过期时间: 7天
 }
